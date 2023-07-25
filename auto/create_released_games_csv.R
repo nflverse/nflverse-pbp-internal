01e6356455d0d0613f6654779517a0c554d8c12c
mrcaseb <mrcaseb@gmail.com>
2023-07-25 21:37:28 +0200

    create released games csv

---
+++ auto/create_released_games_csv.R
@@ -0,0 +1,45 @@
+# CODE TO CREATE THE RELEASED GAMES CSV
+
+
+# Set Data Repo -----------------------------------------------------------
+datarepo <- "nflverse/nflverse-data"
+
+
+# Query all Releases ------------------------------------------------------
+releases <- piggyback::pb_releases(datarepo)
+
+# filter down to raw_pbp release tags
+raw_pbp_releases <- releases |>
+  dplyr::filter(grepl("raw_pbp_", tag_name))
+
+
+# List all files in raw pbp releases --------------------------------------
+datalist <- piggyback::pb_list(datarepo, raw_pbp_releases$tag_name)
+
+# extract game ids from released files
+# this is going to be the released games csv
+games <- datalist |>
+  dplyr::filter(!grepl("timestamp", file_name), grepl(".rds", file_name)) |>
+  dplyr::mutate(
+    game_id = tools::file_path_sans_ext(file_name, compression = TRUE)
+  ) |>
+  dplyr::distinct(game_id, .keep_all = FALSE) |>
+  dplyr::arrange(dplyr::desc(game_id))
+
+
+# Save Released Games csv -------------------------------------------------
+data.table::fwrite(games, "released_games.csv")
+
+
+# Checks ------------------------------------------------------------------
+# This section doesn't need to run to create the file. Just some checks what's
+# missing
+released_games <- nflreadr::load_schedules() |>
+  dplyr::filter(game_id %in% games)
+
+missing_games <- nflreadr::load_schedules() |>
+  dplyr::filter(!game_id %in% games, !is.na(result))
+
+missing_games |> dplyr::count(season)
+
+released_games |> dplyr::count(season)
